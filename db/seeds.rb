d1dee831dd542563857533802c6b9e93e601daeb
moritz gosmann <moritz.gosmann@outlook.de>
2019-11-13 17:00:32 +0100

    controllers, routing and validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Viet Cong',
+    address:      'Charlottenstraße 12, 10178 Berlin',
+    phone_number:  '030 24085251',
+    category:      'chinese'
+  },
+
+   {
+    name:         'Ristorante a Mano',
+    address:      'Friedrichstraße 167, 10117 Berlin',
+    phone_number:  '030 20672828',
+    category:       'italian'
+  },
+   {
+    name:         'Sapa Sushi',
+    address:      'Charlottenstraße 63',
+    phone_number:  '030 20216979',
+    category:       'japanese'
+  },
+   {
+    name:         'Le Saint Amour Restaurant',
+    address:      'Maybachufer 2,12047 Berlin',
+    phone_number:  '030 69564600',
+    category:       'french'
+  },
+   {
+    name:         'Herman',
+    address:      'Schönhauser Allee 173, 10119 Berlin',
+    phone_number:  '0176 22242347',
+    category:       'belgian'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
